2b5a8c9f0c69c52e50eecdde0cde7dcfc2791a87
aarthi-tech29 <aarthi.152529@gmail.com>
2026-05-16 17:28:34 +0530

    Added the tasks

--- ml_tasks/random_forest/Air Pollution Prediction Platform/main.py
+++ ml_tasks/random_forest/Air Pollution Prediction Platform/main.py
@@ -52,7 +52,8 @@ X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
     test_size=0.2,
-    random_state=42
+    random_state=42,
+    stratify=y
 )
 
 # =========================================================
